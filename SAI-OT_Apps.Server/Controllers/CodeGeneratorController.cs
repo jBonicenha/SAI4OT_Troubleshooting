6de8c434f89dffaa503ab5013a1ae023b42eda0d
Guilherme Dinarte Marcondes Lopes <gdlopes@ihm.local>
2024-11-17 16:24:44 -0300

    Adding CodeGenerator feature. Now we have an option to upload an excel file, convert it to a CSV and we send it to the prompt. We plot the XML resultant on the screen

--- SAI-OT_Apps.Server/Controllers/CodeGeneratorController.cs
+++ SAI-OT_Apps.Server/Controllers/CodeGeneratorController.cs
@@ -6,9 +6,9 @@ namespace SAI_OT_Apps.Server.Controllers
     public class CodeGeneratorController : Controller
     {
         private CodeGeneratorService _codeGeneratorService;
-        public CodeGeneratorController()
+        public CodeGeneratorController(CodeGeneratorService codeAuditorServiceUDT)
         {
-            _codeGeneratorService = new CodeGeneratorService(); // Instantiate the service here
+            _codeGeneratorService = codeAuditorServiceUDT;
         }
 
         [HttpPost("generatePLCRoutineCode")]
@@ -41,6 +41,32 @@ namespace SAI_OT_Apps.Server.Controllers
                 Console.WriteLine(ex.Message);
                 return StatusCode(500, "Internal server error: " + ex.Message);
             }
+        }
+
+        [HttpPost("upload-generator-excel")]
+        public async Task<IActionResult> UploadExcel([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            // Chama a service para converter a planilha e obter o caminho do CSV
+            string csvFilePath;
+            using (var stream = file.OpenReadStream())
+            {
+                csvFilePath = _codeGeneratorService.ConvertExcelToCsv(stream);
+            }
+
+            // Lê o conteúdo do arquivo CSV como uma string
+            string csvContent = await System.IO.File.ReadAllTextAsync(csvFilePath);
+
+            // Remove o arquivo temporário após a leitura
+            System.IO.File.Delete(csvFilePath);
+            //Console.WriteLine(csvContent);
+
+            var saiCodeInterlockResult = await _codeGeneratorService.SAICodeGeneratorXML(csvContent);
+
+            // Retorna o conteúdo do CSV como JSON
+            return Ok(saiCodeInterlockResult);
         }
     }
 }
--- sai-ot_apps.client/src/app/code-generator/code-generator.component.ts
+++ sai-ot_apps.client/src/app/code-generator/code-generator.component.ts
@@ -1,70 +1,134 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-code-generator',
   templateUrl: './code-generator.component.html',
-  styleUrl: './code-generator.component.scss'
+  styleUrls: ['./code-generator.component.scss']
 })
-export class CodeGeneratorComponent {
-  constructor(private http: HttpClient, private router: Router) { }
+export class CodeGeneratorComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) { }
 
   loading: boolean = false;
   SAIRoutineCode: string = '';
+  //selectedOption: string = 'create'; // Opção padrão
+  SAICodeGeneratorXML: string | null = null;
+  // Definindo o array de opções para o menu dropdown
+  codeGeneratorMenu: string[] = ['Create Code', 'Use a Code'];
+
+  // Propriedade para armazenar a opção selecionada
+  selectedOption: string = 'Create Code'; // Opção inicial (pode ser 'Create Code')
 
   profileForm: FormGroup = new FormGroup({
     routineName: new FormControl(null),
     userRequest: new FormControl(null),
   });
 
-  resultForm: FormGroup = new FormGroup({
+  resultForm: FormGroup = new FormGroup({});
 
-  });
+  file: File | null = null;
 
   ngOnInit() {
-    // Initialization logic here
+    // Inicializando o formulário
+    this.profileForm = this.fb.group({
+      routineName: [''],
+      userRequest: [''],
+      //codeGeneratorOption: [this.selectedOption]
+      codeGeneratorOption: ['Create Code']
+    });
+
+    this.resultForm = this.fb.group({});
   }
 
+  // Função para alternar entre as opções do menu
+  selectOption(option: string): void {
+    this.selectedOption = option;
+    this.resetForms();
+  }
+
+  // Função chamada ao mudar a opção no dropdown
+  onOptionChange(event: any): void {
+    this.selectedOption = event.target.value;
+    // Aqui você pode fazer o que for necessário quando a opção for alterada, por exemplo:
+    /*if (this.selectedOption === 'create') {
+      // Ação para 'Create Code'
+    } else if (this.selectedOption === 'use') {
+      // Ação para 'Use a Code'
+    }*/
+  }
+
+  // Função de geração de rotina PLC para a opção "Create Code"
   async generatePLCRoutine() {
-    //string routineName, string userResquest
     this.loading = true;
-    var routineName = this.profileForm.get('routineName')?.value
-    var userResquest = this.profileForm.get('userRequest')?.value
+    const routineName = this.profileForm.get('routineName')?.value;
+    const userRequest = this.profileForm.get('userRequest')?.value;
 
-    await this.generatePLCRoutineCode(routineName, userResquest);
-
+    await this.generatePLCRoutineCode(routineName, userRequest);
+    this.loading = false;
   }
 
+  // Função para fazer upload e gerar código usando arquivo na opção "Use a Code"
+  async generateCodeGeneratorXML() {
+    if (this.file) {
+      this.SAICodeGeneratorXML = '';
+      this.loading = true; // Inicia o carregamento
+      const url = `https://localhost:7070/upload-generator-excel`;
+      const formData = new FormData();
+      formData.append('file', this.file, this.file.name);
+      formData.append('folderPath', this.profileForm.get('folderPath')?.value);
+
+      // Define responseType como 'text' para receber uma string no response
+      this.http.post(url, formData, { responseType: 'text' }).subscribe(
+        (data: string) => {
+          // Armazena o conteúdo da resposta em SAICodeAuditorInterlock
+          this.SAICodeGeneratorXML = data;
+          console.log(data);
+
+          // Limpa o estado e formulário após o sucesso
+          this.loading = false;
+          this.profileForm.reset(); // Limpa o formulário após o sucesso
+          this.file = null; // Reseta a variável de arquivo
+          const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement;
+          if (fileInput) {
+            fileInput.value = ''; // Reseta o input de arquivo
+          }
+        },
+        (error) => {
+          console.error('Error occurred:', error);
+          alert('Failed to validate and generate CodeAuditorInterlock');
+          this.loading = false;
+        }
+      );
+    } else {
+      alert('Please select a file before submitting.'); // Validação adicional
+    }
+  }
+
+  // Função de download de código PLC gerado
   async downloadPLCRoutineCode() {
-    var routineName = this.profileForm.get('routineName')?.value
+    const routineName = this.profileForm.get('routineName')?.value;
     await this.downloadPLCCode(routineName);
   }
 
-  //Call the SAI Apps that will generate a intepretation based on the data received
+  // Função para chamada de geração de rotina PLC
   async generatePLCRoutineCode(routineName: string, userRequest: string): Promise<void> {
     const url = `https://localhost:7070/generatePLCRoutineCode?routineName=${encodeURIComponent(routineName)}&userRequest=${encodeURIComponent(userRequest)}`;
     try {
       const data: string = await lastValueFrom(this.http.post<string>(url, null, { responseType: 'text' as 'json' }));
       this.SAIRoutineCode = data || '';
-      this.loading = false;
-      //alert('File generate successfully!');
     } catch (error) {
       console.error(error);
-      this.loading = false;
       alert('Failed to fetch generatePLCRoutineCode');
     }
   }
 
-  //[FromQuery] string routineName, string PLCRoutineCode
-
-  //UpdateRoutineWithComments
+  // Função para download da rotina PLC
   async downloadPLCCode(routineName: string): Promise<void> {
     const url = `https://localhost:7070/downloadPLCRoutineCode?routineName=${encodeURIComponent(routineName)}`;
     try {
-      //const data: string = await lastValueFrom(this.http.post<string>(url, this.SAIRoutineCode, { headers: { 'Content-Type': 'application/json' }, responseType: 'text' as 'json' }));
       const data: string = await lastValueFrom(this.http.post<string>(url, JSON.stringify(this.SAIRoutineCode), { headers: { 'Content-Type': 'application/json' }, responseType: 'text' as 'json' }));
       console.log('Routine downloaded successfully:', data);
     } catch (error) {
@@ -73,6 +137,39 @@ export class CodeGeneratorComponent {
     }
   }
 
+  // Manipulador para selecionar arquivo
+  /*onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) {
+      this.file = input.files[0];
+    }
+  }*/
+
+  onFileSelected(event: any): void {
+    this.file = event.target.files[0];
+  }
+
+  // Função para resetar formulários e estado do arquivo
+  resetForms(): void {
+    this.profileForm.reset();
+    this.file = null;
+    this.SAIRoutineCode = '';
+  }
+
+  downloadXMLFile() {
+    // Verifica se o conteúdo XML existe
+    if (!this.SAICodeGeneratorXML) return;
+
+    // Cria um blob com o conteúdo XML
+    const blob = new Blob([this.SAICodeGeneratorXML], { type: 'application/xml' });
+
+    // Cria uma URL para o blob e aciona o download
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'SAICodeGenerator.L5X'; // Nome do arquivo baixado
+    a.click();
+    window.URL.revokeObjectURL(url); // Limpa a URL para liberar memória
+  }
+
 }
-
-
